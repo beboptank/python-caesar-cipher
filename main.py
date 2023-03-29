ff794d7d833431848e9130cbef0e4cc37d62f6d2
beboptank <beboptank12@gmail.com>
2023-03-29 15:14:19 -0400

    added encrypt function - tests needed

--- main.py
+++ main.py
@@ -5,3 +5,18 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
+
+def encrypt(user_text, user_shift):
+    user_text_list = user_text.split()
+    encrypted_word = []
+
+    for letter in text:
+        alpha_index = alphabet.index(letter)
+
+        encrypted_word.append(alphabet[alpha_index + user_shift])
+
+    encrypted_word = ''.join(encrypted_word)
+    print(encrypted_word)
+
+
+encrypt(text, shift)
--- test/test.py
+++ test/test.py
@@ -1,11 +1,10 @@
 import unittest
 
-
 target = __import__("main.py")
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+        self.assertEqual(target.encrypt("abcde", 1), "bcdef")  # add assertion here
 
 
 if __name__ == '__main__':
